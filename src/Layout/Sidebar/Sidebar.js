719ba3433a5f04650057eecdb12b77222b58b6b5
Ramil' Osmanov <osmanovramil823@gmail.com>
2022-11-03 18:49:43 +0600

    list page is finished

--- src/Layout/Sidebar/Sidebar.js
+++ src/Layout/Sidebar/Sidebar.js
@@ -2,23 +2,28 @@ import React from 'react';
 import "./Sidebar.css";
 import { Link } from "react-router-dom";
 
+import {AiFillFire} from "react-icons/ai";
+import {AiFillStar} from "react-icons/ai";
+import {BsFillGrid1X2Fill} from "react-icons/bs";
+
+
 
 const Sidebar = () => {
-
-
   return (
     <div className='Sidebar col'>
       <div className="container">
         <ul className="nav">
-          <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="/home">Главная</Link>
+          <li className="sidebar-item" to="/home">
+          <AiFillFire className='icon' /> <Link className="nav-link text-dark" aria-current="page" to="/home">Главная</Link>
           </li>
-          <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="/favorites">Избранное</Link>
+          <li className="sidebar-item" to="/favorites">
+            <AiFillStar className='icon' /><Link className="nav-link text-dark" aria-current="page" to="/favorites">Избранное</Link>
           </li>
-          <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="/list">Каталог</Link>
+
+          <li className="sidebar-item d-flex" to="/list">
+            <BsFillGrid1X2Fill className='icon' /><Link className="nav-link text-dark" aria-current="page" to="/list">Каталог</Link>
           </li>
+
         </ul>
       </div>
     </div>
