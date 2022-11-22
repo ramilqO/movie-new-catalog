69da13c685a5f32d56fd050877df9a68576f9147
Ramil' Osmanov <osmanovramil823@gmail.com>
2022-11-23 01:41:09 +0600

    add and remove to favorites button

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,14 +1,33 @@
 import { Button, Card, Container } from 'react-bootstrap';
-import { BsFillBookmarkStarFill } from 'react-icons/bs';
+import { BsFillBookmarkPlusFill, BsFillBookmarkDashFill} from 'react-icons/bs';
+
+import { useState } from 'react';
 
 function Movie({ title, summary, year, poster, rating, id }) {
 
+  let [buttonDisabled, setButtonDisabled] = useState('');
+  let [buttonDelete, setButtonDelete] = useState('d-none');
+
   const pushMovieOnStorage = () => {
     const favorites = JSON.parse(localStorage.getItem('favorites')) || []; //берем массив из localStorage
-    favorites.push({title: title, summary: summary, year: year, poster: poster, rating: rating, id: id}); //пушим в массив фильм
+    favorites.push({title: title, summary: summary, year: year, poster: poster, rating: rating, id: id}); //пушим в массив обьект фильма
     localStorage.setItem('favorites', JSON.stringify(favorites)); //превращаем массив в строку и добавляем в localStorage
 
     console.log(favorites);
+
+    setButtonDisabled('d-none');
+    setButtonDelete('d-block');
+  }
+
+  const deleteMovieFromStorage = () => {
+    const favorites = JSON.parse(localStorage.getItem('favorites'));
+    favorites.pop();
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+
+    console.log(favorites);
+
+    setButtonDisabled('d-block');
+    setButtonDelete('d-none');
   }
 
   return (
@@ -24,7 +43,8 @@ function Movie({ title, summary, year, poster, rating, id }) {
         <Container style={{ display: 'flex', justifyContent: 'space-between', width: "100%", padding: "0", textAlign: "center" }}>
           <Button variant="primary">Смотреть</Button>
           <h3>{rating}</h3>
-          <Button onClick={pushMovieOnStorage}><BsFillBookmarkStarFill /></Button>
+          <Button onClick={pushMovieOnStorage} className={buttonDisabled}><BsFillBookmarkPlusFill /></Button>
+          <Button variant='danger' onClick={deleteMovieFromStorage} className={buttonDelete}><BsFillBookmarkDashFill /></Button>
         </Container>
       </Card.Body>
     </Card>
